d20c678e82b3466380ca648a62239a3c387613af
CM.Bae <gilddongii@gmail.com>
2022-01-26 23:23:59 +0900

    add chapter01

---
+++ Chapter01_BasicStructure/JavaBasicStructure/src/sec01_basicsyntax/EX01_JavaBasicStructure/JavaBasicStructure.java
@@ -0,0 +1,9 @@
+package sec01_basicsyntax.EX01_JavaBasicStructure;
+
+public class JavaBasicStructure {
+	public static void main(String[] args) {
+		//1줄 주석: 화면 출력 코드
+		System.out.println("콘솔 화면에 출력");
+	}
+}
+
