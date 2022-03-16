5fa59e5143fbed3475d51a15db268a39fc15d3f2
CM.Bae <gilddongii@gmail.com>
2022-03-17 00:44:23 +0900

    add

---
+++ ArrayDefinition/ValueAssignment.java
@@ -0,0 +1,12 @@
+public class ValueAssignment {
+    public static void main(String[] args){
+        //배열의 값 대입 방법 1
+        int[] array1 = new int[3];      //배열의 원소 값 강제 초기화
+        array1[0] = 3;
+        array1[1] = 4;
+        array1[2] = 5;
+        System.out.println(array1[0] + " " + array1[1] + " " + array1[2]);
+
+
+    }
+}
