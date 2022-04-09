5aeabd0696bef1d24e07e779a33b722fe8166232
CMBae <gilddongii@gmail.com>
2022-04-09 22:31:02 +0900

    Access Modifier

---
+++ Chapter10_Inheritance/src/sec03_methodoverriding/EX04_MethodOverriding_4/MethodOverriding_4.java
@@ -0,0 +1,23 @@
+// 메서드 오버라이딩과 접근 지정자
+package sec03_methodoverriding.EX04_MethodOverriding_4;
+
+class A{
+    protected void abc(){}
+}
+class B1 extends A{
+    public void abc(){}
+}
+class B2 extends A{
+    protected void abc(){}
+}
+class B3 extends A{
+    // void abc(){}        // default  접근지정자 사용(범위가 좁아져서 불가능)
+}
+class B4 extends A{
+    // private void abc(){}    // 범위가 좁아져서 불가능
+}
+public class MethodOverriding_4 {
+    public static void main(String[] args){
+
+    }
+}
