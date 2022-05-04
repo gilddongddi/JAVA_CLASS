e2cfe32d68dd7762069dcc6e5906f9eaa2633280
CMBae <gilddongii@gmail.com>
2022-05-05 03:14:09 +0900

    add

---
+++ Chapter14_Exception/src/sec01_typeofexception/EX02_UncheckedException/UncheckedException.java
@@ -0,0 +1,30 @@
+// 대표적인 실행 예외
+package sec01_typeofexception.EX02_UncheckedException;
+
+class A{}
+class B extends A{}
+
+public class UncheckedException {
+    public static void main(String[] args){
+
+        // UncheckedException = RuntimeException(실행 예외)
+
+        // 1. ArithmeticException
+        System.out.println(3/0);
+
+        // 2. ClassCastException
+        A a = new A();
+        B b = (B) a;
+
+        // 3. ArrayIndexOutOfBoundException
+        int[] array = {1, 2, 3};
+        System.out.println(array[3]);
+
+        // 4. NumberFormatException
+        int num = Integer.parseInt("10!");
+
+        // 5. NullPointerException
+        String str = null;
+        System.out.println(str.charAt(2));
+    }
+}
